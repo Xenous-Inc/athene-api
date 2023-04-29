cec0b6ecb4b04814738171ecb3a1c1b388232e54
slipneff <88764276+slipneff@users.noreply.github.com>
2023-04-29 22:15:13 +0300

    [Feat] add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { configuration } from './config/configuration';
-
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+    const config = new DocumentBuilder().setTitle('Athene').setVersion('1.0').build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('docs', app, document);
     await app.listen(configuration().port);
 }
 bootstrap();
